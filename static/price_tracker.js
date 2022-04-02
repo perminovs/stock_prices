8bf19d119b2e92ce759cbf6c6e31f8280ade6366
perminovs <perminov-s-i@yandex.ru>
2022-04-02 18:01:34 +0500

    --wip

--- static/price_tracker.js
+++ static/price_tracker.js
@@ -1,10 +1,9 @@
 const CHART_BACKGROUND_COLOR = 'rgb(255, 99, 132)';
 const CHART_BORDER_COLOR = 'rgb(255, 99, 132)';
-const MAX_PRICE_DEEP = 15;  // todo here or back-end?
+const MAX_PRICE_DEEP = undefined;
 
 let priceChart = undefined;
-let lastUpdated = null;
-let isSockerInitialized = false;
+let priceSocket = undefined;
 
 function addData(chart, label, data) {
   chart.data.labels.push(label);
@@ -23,30 +22,36 @@ function removeData(chart) {
 }
 
 function initSocket() {
-  var priceSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/track-price");
+  if (priceSocket) {
+    console.log('closing socket');
+    priceSocket.close();
+  }
+
+  priceSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/track-price");
   
+  priceSocket.onclose = function(){
+    console.log('socket onclose');
+    priceSocket = undefined;
+  }
+
+  priceSocket.onopen = function() {
+      let tickerSelector = document.getElementById("ticker")
+      let tickerName = tickerSelector.value;
+      console.log('send ticker name value tickerSelector.value', tickerName);
+      priceSocket.send(tickerName);
+  }
+
   priceSocket.onmessage = function(event) {
       let tickerData = JSON.parse(event.data);
+      console.log('socket: tickerData =', tickerData);
 
-      tickerData.forEach(item => {
-        if (item.created_at === lastUpdated)
-          return;
+      let time = new Date(tickerData.created_at).toLocaleTimeString();
+      addData(priceChart, time, tickerData.price);
 
-        let time = new Date(item.created_at).toLocaleTimeString();
-        addData(priceChart, time, item.price);
-      });
-
-      if (tickerData.length > 0)
-        lastUpdated = tickerData[tickerData.length - 1].created_at;
-
-      while (priceChart.data.labels.length > MAX_PRICE_DEEP)
+      while (MAX_PRICE_DEEP && priceChart.data.labels.length > MAX_PRICE_DEEP)
         removeData(priceChart);
   };
-  
-  setInterval(function() {
-    let tickerSelector = document.getElementById("ticker")
-    priceSocket.send(JSON.stringify([tickerSelector.value, lastUpdated]));
-  }, 1000);
+  console.log('socket is ready');
 }
 
 function onTickerSelect() {
@@ -62,12 +67,12 @@ function onTickerSelect() {
 function onTickerPriceReceive(response) {
   var labels = [];
   var prices = [];
+  console.log('response for ticker prices', response);
   response.forEach(item => {
     let time = new Date(item.created_at);
     labels.push(time.toLocaleTimeString());
     prices.push(item.price);
   });
-  lastUpdated = response[response.length - 1].created_at;
 
   const data = {
     labels: labels,
@@ -90,10 +95,7 @@ function onTickerPriceReceive(response) {
   
   priceChart = new Chart(canvas, config);
 
-  if (!isSockerInitialized) {
-    initSocket();
-    isSockerInitialized = true;
-  }
+  initSocket();
 }
 
 $(document).ready(function() {
--- stock_prices/views.py
+++ stock_prices/views.py
@@ -1,5 +1,5 @@
 import asyncio
-from datetime import datetime
+from datetime import datetime, timedelta
 from decimal import Decimal
 from functools import lru_cache
 from typing import TYPE_CHECKING, Any, Optional
@@ -23,6 +23,7 @@ class TickerPrice(BaseModel):
     created_at: datetime
 
 
+UPDATE_INTERVAL = timedelta(seconds=1)  # TODO as env
 PRICE_DEEP = 15  # todo here or front-end?
 
 
@@ -39,14 +40,13 @@ def home(request: Request, templates: Jinja2Templates = Depends(get_template)) -
     return templates.TemplateResponse('home.html', {'request': request, 'tickers': ticker_names})
 
 
-def get_ticker_price(ticker_name: str, limit: int = PRICE_DEEP) -> list[TickerPrice]:
+def get_ticker_price(ticker_name: str) -> list[TickerPrice]:
     with db.create_session() as session:
         last_prices: list[db.TickerPrice] = (
             session.query(db.TickerPrice)
             .join(db.Ticker, db.Ticker.id == db.TickerPrice.ticker_id)
             .filter(db.Ticker.name == ticker_name)
             .order_by(db.TickerPrice.id.desc())
-            .limit(limit)
             .all()
         )
         return [TickerPrice(name=ticker_name, price=p.price, created_at=p.created_at) for p in reversed(last_prices)]
@@ -54,29 +54,37 @@ def get_ticker_price(ticker_name: str, limit: int = PRICE_DEEP) -> list[TickerPr
 
 async def ticker_price(websocket: WebSocket) -> None:
     await websocket.accept()
+
+    try:
+        ticker_name = await websocket.receive_text()
+        print(f'listening to changes of {ticker_name = }')
+    except WebSocketDisconnect:
+        return
+
+    previous = None
     while True:
+        last_prices = await asyncio.to_thread(_get_last_price, ticker_name)
+        if previous and previous.created_at <= last_prices.created_at:
+            continue
         try:
-            ticker_name, older_than = await websocket.receive_json()
+            await websocket.send_json(jsonable_encoder(last_prices.dict()))
         except WebSocketDisconnect:
             return
-
-        last_prices = await asyncio.to_thread(_get_price_updates, ticker_name, older_than)
-        if not last_prices:
-            continue
-        await websocket.send_json(last_prices)
+        await asyncio.sleep(UPDATE_INTERVAL.total_seconds())
+        previous = last_prices
 
 
-def _get_price_updates(ticker_name: str, older_than: Optional[datetime]) -> list[dict[str, Any]]:
+def _get_last_price(ticker_name: str) -> Optional[TickerPrice]:
     with db.create_session() as session:
-        last_prices_query = (
+        last_price: Optional[db.TickerPrice] = (
             session.query(db.TickerPrice)
             .join(db.Ticker, db.Ticker.id == db.TickerPrice.ticker_id)
             .filter(db.Ticker.name == ticker_name)
-            .order_by(db.TickerPrice.id.asc())
+            .order_by(db.TickerPrice.id.desc())
+            .limit(1)
+            .first()
         )
-        if older_than:
-            last_prices_query = last_prices_query.filter(db.TickerPrice.created_at > older_than)
-        last_prices: list[db.TickerPrice] = last_prices_query.limit(PRICE_DEEP).all()
-        return jsonable_encoder(
-            [TickerPrice(name=ticker_name, price=p.price, created_at=p.created_at).dict() for p in last_prices]
-        )
+        print(f'{last_price.ticker.name = }, {last_price.price = }, {last_price.created_at = }')
+        if not last_price:
+            return None
+        return TickerPrice(name=ticker_name, price=last_price.price, created_at=last_price.created_at)
